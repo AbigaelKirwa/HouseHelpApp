79a858bba836eff59a8878a88aac70e8a21b600c
AbigaelKirwa <abigael.kirwa@strathmore.edu>
2022-12-07 14:15:00 +0300

    added heroku configurations

--- config.php
+++ config.php
@@ -1,4 +1,17 @@
 <?php
 
 session_start();
-$conn =mysqli_connect("localhost","root","","domesticworkers");
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["us-cdbr-east-06.cleardb.net"];
+$cleardb_username = $cleardb_url["b65d9b4184040e"];
+$cleardb_password = $cleardb_url["33f255c5"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+
+//$conn =mysqli_connect("localhost","root","","domesticworkers");

a04d20f6ff6af1f8a835b0f05ebb0b8de75c00e6
AbigaelKirwa <abigael.kirwa@strathmore.edu>
2022-12-07 14:54:33 +0300

    updated config with heroku details

--- config.php
+++ config.php
@@ -3,11 +3,11 @@
 session_start();
 
 //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_url = parse_url(getenv("mysql://b65d9b4184040e:33f255c5@us-cdbr-east-06.cleardb.net/heroku_0dbbe2971f53a86?reconnect=true"));
 $cleardb_server = $cleardb_url["us-cdbr-east-06.cleardb.net"];
 $cleardb_username = $cleardb_url["b65d9b4184040e"];
 $cleardb_password = $cleardb_url["33f255c5"];
-$cleardb_db = substr($cleardb_url["path"],1);
+$cleardb_db = substr($cleardb_url["mysql://b65d9b4184040e:33f255c5@us-cdbr-east-06.cleardb.net/heroku_0dbbe2971f53a86?reconnect=true"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB

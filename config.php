cda33f456834e43172eb315f50cc14b61f2dbdc5
AbigaelKirwa <abigael.kirwa@strathmore.edu>
2022-12-07 14:58:53 +0300

    updated config file with heroku

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ $cleardb_url = parse_url(getenv("mysql://b65d9b4184040e:33f255c5@us-cdbr-east-06
 $cleardb_server = $cleardb_url["us-cdbr-east-06.cleardb.net"];
 $cleardb_username = $cleardb_url["b65d9b4184040e"];
 $cleardb_password = $cleardb_url["33f255c5"];
-$cleardb_db = substr($cleardb_url["mysql://b65d9b4184040e:33f255c5@us-cdbr-east-06.cleardb.net/heroku_0dbbe2971f53a86?reconnect=true"],1);
+$cleardb_db = substr($cleardb_url["heroku_0dbbe2971f53a86"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB

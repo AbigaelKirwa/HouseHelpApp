ae7a35a744ce1ad7cf11f162be6befe9dca215dc
SamsonMokaya <mokayasamson950@gmail.com>
2023-01-21 10:00:41 -0800

    DB

--- order_history.php
+++ order_history.php
@@ -7,8 +7,6 @@
         $ID = $_SESSION["ID"];
         $result= mysqli_query($conn, "SELECT * FROM users WHERE ID = $ID");
         $row =mysqli_fetch_assoc($result);
-
-
     }else{
         header("Location: signin.php");
     }
@@ -41,7 +39,6 @@
             $h_id = $row['house_help_id']; // example ID
 
             if(isset($_POST['end_service'])){
-                $order_id = $_POST['end_service'];
                 $sql = "UPDATE house_helps SET status = 'Available' WHERE ID = $h_id";
                 mysqli_query($conn, $sql);
             }
@@ -62,25 +59,29 @@
         <p class="text-gray-600">House Number: <?php echo $row["house_number"]; ?></p>
         <p class="text-gray-600">Date: <?php echo $row["date"]; ?></p>
 
+        <?php if ($row2["status"] == "Engaged"){
 
-                <form method="post" id="form">
+            ?>
+             <form method="post" id="form" action="<?php echo $_SERVER['PHP_SELF']; ?>">
                     <button class="btn btn-danger" style="background-color: green; border-radius: 10px; padding: 7px" name="end_service" value="<?php echo $row["house_help_id"]; ?>" id="end_service_btn">End Service</button>
                 </form>  
 
+                <?php
+        }else{
+            ?>
+                 <button class="btn btn-danger" style="background-color: grey; border-radius: 10px; padding: 7px" disabled >End Service</button>
+    
+            <?php
+        }
+        
+        ?>
 
     </div>
-
-
-
     <?php
-
         }
     } else {
-        echo "You dont any orders yet";
+        echo "You dont have any orders yet";
     }
-
-
     ?> 
-    
     </body>
     </html>
